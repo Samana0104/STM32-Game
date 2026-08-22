9c3bfe010e39f730ac566f40a57c41ca673cf056
Samana <b_hb0104@naver.com>
2026-08-22 21:28:23 +0900

    초기환경 구축

---
+++ STM32Game/App/Src/GameMain.c
@@ -0,0 +1,19 @@
+#include "GameMain.h"
+
+int GameMain(void)
+{
+    // // Initialize game components
+    // Game_Init();
+
+    // // Main game loop
+    // while (1)
+    // {
+    //     // Update game state
+    //     Game_Update();
+
+    //     // Render game frame
+    //     Game_Render();
+    // }
+
+    return 0; // This line will never be reached
+}
