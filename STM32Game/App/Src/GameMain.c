da497ce5aec3f38f874fe382443eb65ef11dad4e
rotun <hugent72@gmail.com>
2026-08-25 17:01:59 +0900

    stash

--- STM32Game/App/Src/GameMain.c
+++ STM32Game/App/Src/GameMain.c
@@ -1,9 +1,10 @@
 #include "GameMain.h"
 #include "GCheat.h"
+#include "GLed.h"
 
 static void GameInit(void)
 {
-
+    GledInit();
 }
 
 int GameMain(void)
@@ -15,6 +16,13 @@ int GameMain(void)
     // Main game loop
     while (1)
     {
+        // 1번 LED 켜기
+        SetLedState(LED_ID_1, LED_ON);
+        HAL_Delay(500); // 0.5초 대기
+        
+        // 1번 LED 끄기
+        SetLedState(LED_ID_1, LED_OFF);
+        HAL_Delay(500); // 0.5초 대기
     }
 
     return 0; // This line will never be reached
