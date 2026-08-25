a0692b788bd9db756bb1addfb9b464a3404ae613
samana0104 <b_hb0104@naver.com>
2026-08-25 19:02:20 +0900

    feat : 항상 커맨드 파싱할 수 있도록 추가

--- STM32Game/App/Src/GameMain.c
+++ STM32Game/App/Src/GameMain.c
@@ -1,6 +1,7 @@
 #include "GameMain.h"
 #include "GCheat.h"
 #include "GLed.h"
+#include "GUsart.h"
 
 static void GameInit(void)
 {
@@ -9,21 +10,24 @@ static void GameInit(void)
 
 int GameMain(void)
 {
-    GameInit();
+    char command[UART_COMMAND_SIZE];
 
+    GameInit();
     G_LOG(INFO, "Game started successfully. \r\n");
 
-    // Main game loop
     while (1)
     {
-        // 1번 LED 켜기
+        if (UartReadCommand(command, sizeof(command)))
+        {
+            ParseCommand(command);
+        }
+
         SetLedState(LED_ID_1, LED_ON);
-        HAL_Delay(500); // 0.5초 대기
-        
-        // 1번 LED 끄기
+        HAL_Delay(500);
+
         SetLedState(LED_ID_1, LED_OFF);
-        HAL_Delay(500); // 0.5초 대기
+        HAL_Delay(500);
     }
 
-    return 0; // This line will never be reached
+    return 0;
 }
