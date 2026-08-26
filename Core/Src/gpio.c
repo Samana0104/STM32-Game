e1f9c1e40eb4cac7755a239a393c3f64138c3575
rotun <hugent72@gmail.com>
2026-08-26 17:37:02 +0900

    fix: 핀 배정 및 루프 관련 수정

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -76,14 +76,14 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB10 PB4 PB5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_4|GPIO_PIN_5;
+  /*Configure GPIO pins : PB10 PB3 PB4 PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PA8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  /*Configure GPIO pins : PA8 PA9 PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
--- STM32Game/Bsp/Inc/GButton.h
+++ STM32Game/Bsp/Inc/GButton.h
@@ -18,8 +18,7 @@ typedef enum
     BUTTON_4,
     BUTTON_5,
     BUTTON_6,
-    BUTTON_7,
-    BUTTON_START
+    BUTTON_7
 } ButtonId;
 
 
