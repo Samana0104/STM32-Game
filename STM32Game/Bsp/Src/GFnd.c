384fcbe5f05f099f2f43719c4dd9c3cd55bca5f4
rotun <hugent72@gmail.com>
2026-08-26 19:20:06 +0900

    fiX: 세그먼트 칩 사용

--- STM32Game/Bsp/Src/GFnd.c
+++ STM32Game/Bsp/Src/GFnd.c
@@ -1,5 +1,6 @@
 #include "GFnd.h"
 
+/* 자릿수 선택 핀 */
 #define DIGIT_PORT     GPIOC
 #define DIG_SINGLE_PIN GPIO_PIN_6 /* 1자리 FND COM */
 #define DIG1_PIN       GPIO_PIN_2 /* 4자리 FND 1000의 자리 */
@@ -7,35 +8,54 @@
 #define DIG3_PIN       GPIO_PIN_4 /* 4자리 FND 10의 자리 */
 #define DIG4_PIN       GPIO_PIN_5 /* 4자리 FND 1의 자리 */
 
+/* FND 제어용 74HC595 핀 */
+#define FND_DATA_PORT  GPIOB
+#define FND_DATA_PIN   GPIO_PIN_0
+#define FND_CLOCK_PORT GPIOB
+#define FND_CLOCK_PIN  GPIO_PIN_1
+#define FND_LATCH_PORT GPIOB
+#define FND_LATCH_PIN  GPIO_PIN_2
+
+/* Common Cathode 기준 폰트 테이블 */
 static const uint8_t segmentTable[10] = {
-    0x3F, /* 0 */
-    0x06, /* 1 */
-    0x5B, /* 2 */
-    0x4F, /* 3 */
-    0x66, /* 4 */
-    0x6D, /* 5 */
-    0x7D, /* 6 */
-    0x07, /* 7 */
-    0x7F, /* 8 */
-    0x6F  /* 9 */
+    0x3F, /* 0 (A B C D E F) */
+    0x06, /* 1 (B C) */
+    0x5B, /* 2 (A B D E G) */
+    0x4F, /* 3 (A B C D G) */
+    0x66, /* 4 (B C F G) */
+    0x6D, /* 5 (A C D F G) */
+    0x7D, /* 6 (A C D E F G) */
+    0x07, /* 7 (A B C) */
+    0x7F, /* 8 (A B C D E F G) */
+    0x6F  /* 9 (A B C D F G) */
 };
 
 static uint8_t displayDigits[FND_TOTAL_DIGITS] = {0, 0, 0, 0, 0};
 static uint8_t currentDigitIndex = 0;
 
-static void WriteSegments(uint8_t byte)
+static void ShiftOutSegmentData(uint8_t segByte)
 {
-    /* A: PB0, B: PA6, C: PA7, D: PB1 */
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0,  (byte & 0x01) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6,  (byte & 0x02) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7,  (byte & 0x04) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1,  (byte & 0x08) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    /* Q7(DP)부터 Q0(A)까지 MSB First 전송 */
+    for (int i = 7; i >= 0; i--)
+    {
+        HAL_GPIO_WritePin(FND_CLOCK_PORT, FND_CLOCK_PIN, GPIO_PIN_RESET);
 
-    /* E: PB2, F: PB6, G: PB7, DP: PB12 */
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2,  (byte & 0x10) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,  (byte & 0x20) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7,  (byte & 0x40) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, (byte & 0x80) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+        if ((segByte >> i) & 0x01)
+        {
+            HAL_GPIO_WritePin(FND_DATA_PORT, FND_DATA_PIN, GPIO_PIN_SET);
+        }
+        else
+        {
+            HAL_GPIO_WritePin(FND_DATA_PORT, FND_DATA_PIN, GPIO_PIN_RESET);
+        }
+
+        HAL_GPIO_WritePin(FND_CLOCK_PORT, FND_CLOCK_PIN, GPIO_PIN_SET);
+    }
+
+    /* 래치 클록 발생 (출력 갱신) */
+    HAL_GPIO_WritePin(FND_LATCH_PORT, FND_LATCH_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(FND_LATCH_PORT, FND_LATCH_PIN, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(FND_LATCH_PORT, FND_LATCH_PIN, GPIO_PIN_RESET);
 }
 
 static void SelectDigit(uint8_t index)
@@ -55,7 +75,7 @@ void FndInit(void)
 
 void ClearFnd(void)
 {
-    WriteSegments(0x00);
+    ShiftOutSegmentData(0x00);
 }
 
 void SetFndSingleDigit(uint8_t digit)
@@ -82,11 +102,13 @@ void SetFnd4DigitNumber(uint16_t number)
 
 void UpdateFnd(void)
 {
-    ClearFnd();
-    SelectDigit(currentDigitIndex);
+    /* 잔상 방지를 위해 자릿수 전환 전 소등 */
+    SelectDigit(0xFF);
 
     uint8_t digitVal = displayDigits[currentDigitIndex];
-    WriteSegments(segmentTable[digitVal]);
+    ShiftOutSegmentData(segmentTable[digitVal]);
+
+    SelectDigit(currentDigitIndex);
 
     currentDigitIndex = (currentDigitIndex + 1) % FND_TOTAL_DIGITS;
 }
